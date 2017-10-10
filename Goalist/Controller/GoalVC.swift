157601b70301e2b1029e86222e8edfcd667749b7
kevinvud <kevinvud@gmail.com>
2017-10-10 07:47:47 -0700

    Set up UI

---
+++ Goalist/Controller/GoalVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Goalist
+//
+//  Created by PoGo on 10/10/17.
+//  Copyright © 2017 PoGo. All rights reserved.
+//
+
+import UIKit
+
+class GoalVC: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addGoalButtonPressed(_ sender: Any) {
+    }
+    
+}
+
